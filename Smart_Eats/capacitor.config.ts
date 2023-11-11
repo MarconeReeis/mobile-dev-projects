9881c5e90b6ef44a6d3045406e471ea213f0ff37
Marcone Reis <marcone.trajano.reis@gmail.com>
2023-11-11 14:08:12 -0300

    Estrutura inicial e header

--- Smart_Eats/capacitor.config.ts
+++ Smart_Eats/capacitor.config.ts
@@ -2,7 +2,7 @@ import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'SmartEats',
+  appName: 'Smart_Eats',
   webDir: 'www',
   server: {
     androidScheme: 'https'
--- Smart_Eats/src/app/pages/home/home.module.ts
+++ Smart_Eats/src/app/pages/home/home.module.ts
@@ -1,19 +1,21 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { IonicModule } from '@ionic/angular';
 import { FormsModule } from '@angular/forms';
-import { HomePage } from './home.page';
+
+import { IonicModule } from '@ionic/angular';
 
 import { HomePageRoutingModule } from './home-routing.module';
 
+import { HomePage } from './home.page';
+import { HeaderComponent } from 'src/app/components/header/header.component';
 
 @NgModule({
   imports: [
     CommonModule,
     FormsModule,
     IonicModule,
-    HomePageRoutingModule
+    HomePageRoutingModule,
   ],
-  declarations: [HomePage]
+  declarations: [HomePage,HeaderComponent]
 })
 export class HomePageModule {}
